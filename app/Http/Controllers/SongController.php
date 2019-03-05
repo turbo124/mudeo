adb92df5f7b49247439d47eea4d8d6aeb2bd5918
David Bomba <turbo124@gmail.com>
2019-03-05 21:05:28 +1100

    Models

---
+++ app/Http/Controllers/SongController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Song;
+use Illuminate\Http\Request;
+
+class SongController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Song  $song
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Song $song)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Song  $song
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Song $song)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Song  $song
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Song $song)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Song  $song
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Song $song)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TrackController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Track;
+use Illuminate\Http\Request;
+
+class TrackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Track $track)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Track $track)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Track $track)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Track $track)
+    {
+        //
+    }
+}
