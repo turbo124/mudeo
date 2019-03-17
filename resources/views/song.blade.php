a18a95fdd1f9f4c545cba59db065ceed555b1f85
David Bomba <davidbomba@Davids-iMac.local>
2019-03-18 09:34:00 +1100

    Working on web view

--- resources/views/song.blade.php
+++ resources/views/song.blade.php
@@ -10,9 +10,14 @@
 
 		    <div class="d-flex justify-content-center">
 
-				<video id="player" playsinline controls>
-					<source src="{{ $video_url }}" type="video/mp4" />
-				</video>
+				  <video id='my-video' class='video-js' controls preload='auto' width='640' height='264'
+				  poster='' data-setup='{}'>
+				    <source src='{{ $video_url }}' type='video/mp4'>
+				    <p class='vjs-no-js'>
+				      To view this video please enable JavaScript, and consider upgrading to a web browser that
+				      <a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
+				    </p>
+				  </video>
 
 			</div>
 
