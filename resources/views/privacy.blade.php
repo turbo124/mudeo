01aeae2702dc730dbf3f6ada5ab8154747ae6d19
Hillel Coren <hillelcoren@gmail.com>
2019-03-17 20:54:26 +0200

    Updated terms/privacy

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,30 @@
+<h3>Privacy Policy, Consent to Process, Data Control</h3>
+
+<p/>
+This DPA (Data Privacy Addendum) applies to websites invoiceninja.com, app.invoiceninja.com including all subdomains, and invoiceninja.org (the “Sites”) owned and operated by mudeo, LLC (collectively, “mudeo”, “we”, “us”, or “our”). This Privacy Policy describes how mudeo collects, uses, shares and secures the personal data you provide to us. It also describes your options in deleting/purging your data permanently from mudeo in compliance with GDPR.
+
+<p/>
+Consent: PII Data We Collect<br/>
+User Namei<br/>
+User Email<br/>
+User IP<br/>
+
+<p/>
+GDPR Requires that mudeo disclose 3rd party vendors which interact with your data. In order to operate the mudeo system, the following parties receive partial data that you provide, as outlined in the mudeo ‘Terms of Service’: 
+
+<p/>
+Cloudflare<br/>
+Linode<br/>
+Google Storage<br/>
+Postark<br/>
+
+<p/>
+Restriction/Erasure: Purging PII Account Data
+The below three methods of data purge are final, total, and irreversible.
+
+<p/>
+Communication & Resolution
+Users with questions regarding their account privacy, data use, data purging, questions or concerns on any privacy or any data use matter, should contact: mudeo.app@gmail.com
+
+ 
+
---
+++ resources/views/terms.blade.php
@@ -0,0 +1,99 @@
+<h3>Mudeo Terms of Use</h3>
+<h4>Introduction</h4>
+
+Below is anagreement between you (the User) and Mudeo.app ("Mudeo", "our" or “the app”) which sets out the terms of use for your use of mudeo = MUsic + video app, available for Google Play & iOS, including data, media, and services available through the app. All terminology for “music” & “video” are defined within and used interchangeably below as “content.”
+<p/>
+
+By downloading and/or using the app you agree to these legally binding terms. If you do not agree to the Terms, you must not download and/or stop using the Mudeo app.
+<p/>
+
+We license use of the App to you on the basis of these Terms and subject to the rules or policies applied by Google Play Terms of Service &/or the Apple App Store Terms & Conditions from where you may download the App. We remain the owners of the App and all rights in it at all times.
+<p/>
+
+If you have any comments or questions relating to the App, please email mudeo.app@gmail.com
+<p/>
+
+<h4>Music Creation</h4>
+<ol>
+<li>The app allows you to create and record music & video. You are solely responsible for all your content from the point of creation and, to the extent permitted by law, we exclude all liability (including liability for copyright infringement) with respect to your content and the activities of any other users of these. 
+</li>
+<li>
+You retain all ownership rights in content that you create. You grant us an irrevocable, perpetual, worldwide, non-exclusive, royalty-free, transferable license to use, store, upload, modify, reproduce, display and perform the content as necessary to allow you to share your content with other users of the App. 
+</li>
+<li>
+The Music you create will be considered non-confidential and non-proprietary.
+</li>
+<li>
+You confirm that such music created or uploaded to the app do not violate the Terms or any third-party agreement, applicable law or the intellectual property rights of any third party. You agree to indemnify and hold us harmless of any loss or damage we suffer as a result of your breach of this clause.
+</li>
+<li>
+You agree and acknowledge that, although we have no obligation to do so, we may in our sole discretion monitor, review, edit, or remove content that we consider inappropriate, that does or might infringe any third party rights.
+</li>
+</ol>
+
+<h4>User License</h4>
+<ol>
+<li>
+We grant you a non-exclusive license to download the app to your device for its intended use. This user license is granted for personal, non-commercial use.
+</li>
+<li>
+You acknowledge that all intellectual property/content in the app belong to us or our licensors, that rights in the app are licensed (not sold) to you for commercial use or redistribution, unless you have been explicitly granted additional rights by the relevant rights holder.
+</li>
+<li>
+You acknowledge that you have no right to have access to the app in source-code form.
+</li>
+</ol>
+
+<h4>General</h4>
+<ol>
+<li>
+We provide hosting services as necessary to enable you to create and share your content with other users of the app. This is not intended as a back-up service for or a storage service for your music. 
+</li>
+<li>
+We have the right, without penalty to us, to delete any music or content uploaded to our servers at any time and without notice to you. 
+</li>
+<li>
+If you believe that any music in the app infringe your rights or intellectual property rights, please contact us immediately with all details of the alleged claim. While we are committed to dealing with all complaints immediately, you acknowledge that our ability to identify particular music is limited due to anonymity with which music is created and uploaded.
+</li>
+</ol>
+
+<h4>Acceptable Use Restrictions</h4>
+<ol>
+You agree in particular to refrain from doing any of the following:<p/>
+<li>
+Use the app in any unlawful manner or in any manner inconsistent with these Terms, or act fraudulently or maliciously, for example, by hacking into or uploading malicious code or harmful data, into the app;
+</li>
+<li>
+Infringe our intellectual property rights or those of any third party in relation to your use of the app;
+</li>
+<li>
+Use the app in a way that could damage, disable, overburden, impair or compromise our systems or security or interfere with other users;
+</li>
+</ol>
+
+<h4>Warranties</h4>
+<ol>
+<li>
+The App is provided to you on an “as is” and “as available” basis. We do not guarantee that your use of the App, or any content on it, will be uninterrupted or error-free. You further acknowledge that neither we, nor the App Store, nor Google Play, have obligation to provide maintenance or support to the app.
+Limitation of Liability
+</li>
+<li>
+We will not be liable or responsible for any failure to perform, or delay in performance of, any of our obligations under these terms that is caused by any act or event beyond our reasonable control or by any 3rd party.  In such circumstances, our obligations will be suspended and we will use our reasonable endeavours to find a solution to the act or event.
+</li>
+<li>
+Our maximum aggregate liability under or in connection with these Terms whether in contract, tort (including negligence) or otherwise, shall in all circumstances be limited to the subscription fees (if any) paid by you via App Store or Google Play for using the App during the calendar year preceding the damage.
+</li>
+</ol>
+
+<h4>Termination</h4>
+<ol>
+<li>
+We may terminate these terms immediately and without notice if you commit a material or persistent breach of the Terms. It is hereby recognised that inter alia a breach of any of the Licence Restrictions or the Acceptable Use Restrictions shall qualify as a material breach.
+</li>
+<li>
+You may terminate these Terms immediately at any time by uninstalling the app from your device and stop using the App.
+</li>
+<li>
+Upon termination or expiry for whichever reason, all rights granted to you under these terms shall cease. You must immediately cease all activities authorised by these terms, including your use of the App and delete or remove the app from all devices.
+</li>
+</ol>
