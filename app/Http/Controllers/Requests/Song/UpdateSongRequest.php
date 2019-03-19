9e8d59718f0c1ba86d5b7614bf586c456cbd6c03
David Bomba <davidbomba@Davids-iMac.local>
2019-03-20 08:47:53 +1100

    update song

---
+++ app/Http/Controllers/Requests/Song/UpdateSongRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Song;
+
+use App\Http\Requests\Request;
+use App\Models\Song;
+use Illuminate\Support\Facades\Hash;
+
+class UpdateSongRequest extends Request
+{
+/*
+    public function authorize()
+    {
+        return $this->user()->id === $this->song->user_id;
+    }
+  */  
+    public function rules()
+    {
+        $this->sanitize();
+
+        return [
+            //'email' => 'required|unique:users|string|email|max:100',
+            //'handle' => 'required|unique:users|max:100',
+           // 'first_name'        => 'required|string|max:100',
+           // 'last_name'         =>  'required|string:max:100',
+            //'password'          => 'required|string|min:6',
+        ];
+    }
+
+    public function sanitize()
+    {
+        $input = $this->all();
+        
+        $input['user_id'] = auth()->user()->id;
+        $input['description'] = isset($input['description']) ? $input['description'] : ' ';
+        $input['title'] = isset($input['title']) ? $input['title'] : ' ';
+        
+        $this->replace($input);     
+    }
+
+}
