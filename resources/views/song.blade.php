29228123651058c392a52c3a8799cfa34abff115
Hillel Coren <hillelcoren@gmail.com>
2019-04-28 08:35:16 +0300

    Update song view

--- resources/views/song.blade.php
+++ resources/views/song.blade.php
@@ -1,39 +1,34 @@
 @extends('layouts.mudeo')
 
-	@section('body')
+@section('body')
+	<style>
+		body {background-color:black}
+	</style>
 
-		<div class="container-fluid">
-		    
-		    <div class="d-flex justify-content-center">
-		    	<h1>{{ $song->title }}</h1>
-		    </div>
+	<p></p>
 
-		    <div class="d-flex justify-content-center">
 
-				  <video id='my-video' class='video-js' controls preload='auto' 
-				  poster='' data-setup='{}'>
-				    <source src='{{ $video_url }}' type='video/mp4'>
-				    <p class='vjs-no-js'>
-				      To view this video please enable JavaScript, and consider upgrading to a web browser that
-				      <a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
-				    </p>
-				  </video>
+	<center>
+		<a href="https://mudeo.app" target="_blank" style="font-weight:100">DOWNLOAD THE APP</a><p/>
+	</center>
 
-			</div>
 
-		    <div class="d-flex justify-content-center">
-		    	<p>{{ $song->description }}</p>
-		    </div>
-
+	<div class="container-fluid">
+		<div class="d-flex justify-content-center">
+			<video id='my-video' class='video-js vjs-default-skin vjs-big-play-centered' autoplay controls preload='auto' poster='' data-setup='{}'>
+				<source src='{{ $video_url }}' type='video/mp4'>
+					<p class='vjs-no-js'>
+						To view this video please enable JavaScript, and consider upgrading to a web browser that
+						<a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
+					</p>
+			</video>
 		</div>
+	</div>
 
-<script>
 
+	<script>
+		var player = videojs('my-video');
+		player.fluid(true);
+	</script>
 
-	var player = videojs('my-video');
-
-player.fluid(true);
-</script>
-
-	@endsection
-
+@endsection
