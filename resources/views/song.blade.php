409148ebb6b7c214a72bae92e622639e59c26429
Hillel Coren <hillelcoren@gmail.com>
2019-12-29 19:47:24 +0200

    Update ffmpeg params

--- resources/views/song.blade.php
+++ resources/views/song.blade.php
@@ -37,17 +37,13 @@
 	</center>
 
 
-	<div class="container-fluid">
-		<div class="d-flex justify-content-center">
-			<video id='my-video' class='video-js vjs-default-skin vjs-big-play-centered' autoplay controls preload='auto' poster='' data-setup='{}'>
-				<source src='{{ $video_url }}' type='video/mp4'>
-					<p class='vjs-no-js'>
-						To view this video please enable JavaScript, and consider upgrading to a web browser that
-						<a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
-					</p>
-			</video>
-		</div>
-	</div>
+	<video id='my-video' class='video-js vjs-default-skin vjs-big-play-centered' autoplay controls preload='auto' poster='' data-setup='{}'>
+		<source src='{{ $video_url }}' type='video/mp4'>
+			<p class='vjs-no-js'>
+				To view this video please enable JavaScript, and consider upgrading to a web browser that
+				<a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
+			</p>
+	</video>
 
 
 	<script>
