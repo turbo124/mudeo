bb4c724604e45ac4557a04e041aa3008a9f33b3d
Hillel Coren <hillelcoren@gmail.com>
2020-03-30 17:29:57 +0300

    Added youtube lib

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Dawson\Youtube\YoutubeServiceProvider::class,
     ],
 
     /*
@@ -226,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Youtube' => Dawson\Youtube\Facades\Youtube::class
     ],
 
 ];
