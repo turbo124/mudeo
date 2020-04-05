08557d55322b0767f672f84f87f1dcfbc828fb91
Hillel Coren <hillelcoren@gmail.com>
2020-04-05 14:39:50 +0300

    Analytics

--- resources/views/layouts/mudeo.blade.php
+++ resources/views/layouts/mudeo.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('ninja.analytics_id') }}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '{{ config('ninja.analytics_id') }}');
+    </script>
 
+    <meta charset="UTF-8">
     <title>mudeo</title>
     <meta content="IE=Edge" http-equiv="X-UA-Compatible">
     <meta name="description" content="A new Flutter project.">
     <link rel="shortcut icon" type="image/png" href="https://mudeo.app/images/icon.png"/>
-    
+
     @yield('head')
 </head>
 
