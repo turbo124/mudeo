17ad95fb52ea13bff6f186b1fc70977834297cc4
Hillel Coren <hillelcoren@gmail.com>
2020-04-28 17:56:59 +0300

    Clean formatting

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,14 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-
     /**
      *
      */
     public function index()
     {
-	if (config('mudeo.is_dance')) {	
-		return redirect('https://play.google.com/apps/testing/app.mudeo.dancelikeme');
-	}
+    	if (config('mudeo.is_dance')) {
+    		return redirect('https://play.google.com/apps/testing/app.mudeo.dancelikeme');
+    	}
 
         return view('flutter');
     }
