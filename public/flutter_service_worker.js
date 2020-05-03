a37ca2f120d4cae0a0472ade070532bb6ab9efec
Hillel Coren <hillelcoren@gmail.com>
2020-05-03 16:06:07 +0300

    Update Flutter build

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -9,7 +9,7 @@ const RESOURCES = {
 "assets/assets/posenet_mv1_075_float_from_checkpoints.tflite": "e0c83d992292731a3f2dfe387d1470a6",
 "assets/FontManifest.json": "667bf07c9f50ab329c7c28259428c7a4",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "6c326fcefbff145cfb4b8f0272d81ea0",
+"assets/LICENSE": "cca6ce3ed4a2986da29e112ce2508baf",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
@@ -17,9 +17,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "5b1ae5e5bec4e289df11cb053ed1031d",
-"/": "5b1ae5e5bec4e289df11cb053ed1031d",
-"main.dart.js": "eb627bf80b91edf901e9bce5bdc50f7d",
+"index.html": "ce4db044adeb59677d5256f951240a08",
+"/": "ce4db044adeb59677d5256f951240a08",
+"main.dart.js": "fa321adfc3e553ae00ec01a70dd0f67b",
 "manifest.json": "2029e9f86a9559a5c109661cbe207dee"
 };
 
