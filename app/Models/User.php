8b488027c1a63d25568b0153f723ff6f7f5edd48
Hillel Coren <hillelcoren@gmail.com>
2020-05-24 19:31:36 +0300

    bug fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable implements CanResetPassword
         'updated_at',
         'created_at',
         'deleted_at',
+        'include',
         'q',
         'api_secret',
         'server_name',
