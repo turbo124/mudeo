d586c605fadfab4660673f21e6ebc79ea1f26687
Hillel Coren <hillelcoren@gmail.com>
2020-05-24 15:51:20 +0300

    Share songs

--- app/Notifications/SongJoined.php
+++ app/Notifications/SongJoined.php
@@ -26,9 +26,9 @@ class SongJoined extends Notification
         $song = $this->song;
 
         return (new MailMessage)
-                    ->subject($user->name . ' has joined ' . $song->title . ' 🎉')
+                    ->subject($user->name . ' has joined ' . $song->title)
                     ->greeting('Hello!')
-                    ->line('Your song ' . $song->title . ' has a new collaborator')
+                    ->line('Your song ' . $song->title . ' has a new collaborator 🎉')
                     ->line($user->name . ' - @' . $user->handle);
     }
 }
